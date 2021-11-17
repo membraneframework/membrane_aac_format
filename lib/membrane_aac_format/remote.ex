ab2f51f3a90bbbc95dfdd2b5f16358116e6268b1
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-11-17 12:03:55 +0100

    Better description

--- lib/membrane_aac_format/remote.ex
+++ lib/membrane_aac_format/remote.ex
@@ -1,6 +1,11 @@
 defmodule Membrane.RemoteStream.AAC do
   @moduledoc """
-  Format definition for Remote AAC Stream with known audio decoder configuration
+  Format definition for unparsed AAC stream with audio decoder configuration transmitted out-of-band.
+
+  Example of such a stream is AAC depayloaded from a container like MP4 or FLV, where audio specific config
+  is signalled outside of the AAC bytestream.
+
+  `encapsulation: :none` should be assumed.
   """
 
   @enforce_keys [:audio_specific_config]
