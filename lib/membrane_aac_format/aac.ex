25582f5193130f9672f402d9ad590d3453edb564
Noarkhh <jakub.pryc@gmail.com>
2023-08-23 12:13:46 +0200

    Add mpeg version id conversion

--- lib/membrane_aac_format/aac.ex
+++ lib/membrane_aac_format/aac.ex
@@ -4,8 +4,15 @@ defmodule Membrane.AAC do
   """
 
   @type profile :: :main | :LC | :SSR | :LTP | :HE | :HEv2
-  @type mpeg_version :: 2 | 4
   @type samples_per_frame :: 1024 | 960
+  @type mpeg_version :: 2 | 4
+
+  @typedoc """
+  MPEG version identifiers.
+
+  `0` indicates MPEG-4 and `1` indicates MPEG-2
+  """
+  @type mpeg_version_id :: 0 | 1
 
   @typedoc """
   Indicates whether stream contains AAC frames only or are they encapsulated
@@ -39,19 +46,19 @@ defmodule Membrane.AAC do
   Contains the configuration data of the audio stream. The options are:
   * Elementary Stream Descriptor, `:esds` - ISO/IEC 14496-14
   * Audio Specific Config, `:audio_specific_config` - ISO/IEC 14496-3
-  If the data is already parsed it can be set to nil.
+  If the data is already parsed and the stream doesn't contain external config it can be set to nil.
   """
-  @type config :: {:esds | :audio_specific_config, binary()}
+  @type config :: {:esds | :audio_specific_config, binary()} | nil
 
   @type t :: %__MODULE__{
           profile: profile() | nil,
           mpeg_version: mpeg_version(),
           sample_rate: pos_integer() | nil,
           channels: pos_integer() | nil,
-          samples_per_frame: 1024 | 960,
+          samples_per_frame: samples_per_frame(),
           frames_per_buffer: pos_integer(),
           encapsulation: encapsulation(),
-          config: config() | nil
+          config: config()
         }
 
   defstruct sample_rate: nil,
@@ -63,7 +70,7 @@ defmodule Membrane.AAC do
             encapsulation: :none,
             config: nil
 
-  defp audio_object_type(),
+  defp audio_object_type_map(),
     do:
       BiMap.new(%{
         1 => :main,
@@ -74,7 +81,7 @@ defmodule Membrane.AAC do
         29 => :HEv2
       })
 
-  defp sampling_frequency(),
+  defp sampling_frequency_map(),
     do:
       BiMap.new(%{
         0 => 96_000,
@@ -93,7 +100,7 @@ defmodule Membrane.AAC do
         15 => :explicit
       })
 
-  defp channel_config(),
+  defp channel_config_map(),
     do:
       BiMap.new(%{
         0 => :AOT_specific,
@@ -106,42 +113,57 @@ defmodule Membrane.AAC do
         7 => 8
       })
 
-  defp frame_length(),
+  defp frame_length_map(),
     do:
       BiMap.new(%{
         0 => 1024,
         1 => 960
       })
 
+  defp mpeg_version_map(),
+    do:
+      BiMap.new(%{
+        0 => 4,
+        1 => 2
+      })
+
   @spec aot_id_to_profile(audio_object_type_id()) :: profile()
   def aot_id_to_profile(audio_object_type_id),
-    do: BiMap.fetch!(audio_object_type(), audio_object_type_id)
+    do: BiMap.fetch!(audio_object_type_map(), audio_object_type_id)
 
   @spec profile_to_aot_id(profile()) :: audio_object_type_id()
-  def profile_to_aot_id(profile), do: BiMap.fetch_key!(audio_object_type(), profile)
+  def profile_to_aot_id(profile), do: BiMap.fetch_key!(audio_object_type_map(), profile)
 
   @spec sampling_frequency_id_to_sample_rate(sampling_frequency_id()) :: pos_integer()
   def sampling_frequency_id_to_sample_rate(sampling_frequency_id),
-    do: BiMap.fetch!(sampling_frequency(), sampling_frequency_id)
+    do: BiMap.fetch!(sampling_frequency_map(), sampling_frequency_id)
 
   @spec sample_rate_to_sampling_frequency_id(sample_rate :: pos_integer() | :explicit) ::
           sampling_frequency_id()
   def sample_rate_to_sampling_frequency_id(sample_rate),
-    do: BiMap.fetch_key!(sampling_frequency(), sample_rate)
+    do: BiMap.fetch_key!(sampling_frequency_map(), sample_rate)
 
   @spec channel_config_id_to_channels(channel_config_id()) :: pos_integer() | :AOT_specific
   def channel_config_id_to_channels(channel_config_id),
-    do: BiMap.fetch!(channel_config(), channel_config_id)
+    do: BiMap.fetch!(channel_config_map(), channel_config_id)
 
   @spec channels_to_channel_config_id(channels :: pos_integer() | :AOT_specific) ::
           channel_config_id()
-  def channels_to_channel_config_id(channels), do: BiMap.fetch_key!(channel_config(), channels)
+  def channels_to_channel_config_id(channels), do: BiMap.fetch_key!(channel_config_map(), channels)
 
   @spec frame_length_id_to_samples_per_frame(frame_length_id()) :: samples_per_frame()
   def frame_length_id_to_samples_per_frame(frame_length_id),
-    do: BiMap.fetch!(frame_length(), frame_length_id)
+    do: BiMap.fetch!(frame_length_map(), frame_length_id)
 
   @spec samples_per_frame_to_frame_length_id(samples_per_frame()) :: pos_integer()
   def samples_per_frame_to_frame_length_id(samples_per_frame),
-    do: BiMap.fetch_key!(frame_length(), samples_per_frame)
+    do: BiMap.fetch_key!(frame_length_map(), samples_per_frame)
+
+  @spec mpeg_version_to_mpeg_version_id(mpeg_version()) :: mpeg_version_id()
+  def mpeg_version_to_mpeg_version_id(mpeg_version),
+    do: BiMap.fetch_key!(mpeg_version_map(), mpeg_version)
+
+  @spec mpeg_version_id_to_mpeg_version(mpeg_version_id()) :: mpeg_version()
+  def mpeg_version_id_to_mpeg_version(mpeg_version_id),
+    do: BiMap.fetch!(mpeg_version_map(), mpeg_version_id)
 end
